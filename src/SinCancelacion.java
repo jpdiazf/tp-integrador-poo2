2cd6b465a166ff4739ae9517757d95d4993fe709
Daniel <daniel.barberena10@gmail.com>
2021-06-17 17:29:21 -0300

    Subo SinCancelacion

---
+++ src/SinCancelacion.java
@@ -0,0 +1,21 @@
+
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class SinCancelacion extends Cancelacion {
+
+	//Sin cancelación: en caso de cancelar el usuario de todas formas paga
+	//los días que había reservado.
+
+	public SinCancelacion() {
+		super();
+	}
+	
+	@Override
+	double montoCancelacion(Reserva reserva, LocalDate fecha) {
+		return reserva.valor();
+	}
+	
+	
+}
